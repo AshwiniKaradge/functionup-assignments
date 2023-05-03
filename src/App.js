183c1cf85fa714e5d8fb9e36d160ca9a07297488
AshwiniKaradge <ashwinikaradge2018@gmail.com>
2023-05-03 11:19:51 +0530

    this is featch of ueser id

--- src/App.js
+++ src/App.js
@@ -1,60 +1,42 @@
 
-import './App.css';
-import React, {useState, useEffect} from 'react';
+import React, { useState } from 'react';
 
-function App() {
-
-  const [image , setImage] = useState('https://images.pexels.com/photos/3177388/pexels-photo-3177388.jpeg?auto=compress&cs=tinysrgb&w=800')
-  const url="http://localhost:3001/"
-  const WAIT_TIME = 2000;
-  useEffect(()=> {
-    const id= setInterval(()=>{
-      setImage
-      .get(url)
-      .then(res=>{
-        setImage(res.data)
-      })
-      .catch(err=>{
-        console.log(err);
-      })
-    }, WAIT_TIME);
-    return ()=> clearInterval(id)
-  }, [image]);
-  async function getDogData () {
-    
-    fetch('https://dog.ceo/api/breeds/image/random')
-    .then((data)=> data.json())
-    .then ((response)=> setImage(response.message))
-    .catch((error) => console.log(error))  
-
-  }
-
-  async function getDog () {
-
-    try{
-          const response = await fetch('https://dog.ceo/api/breeds/image/random')
-        const data = await response.json()
-        setImage(data.message)
-    }
-    catch(error){
-      console.log('i am from catch' ,error)
-    }
-
-
-  }
+function UserCard({ user }) {
   return (
-    <div className="App">
-      <img 
-        src={image}
-        height={'200px'}
-        width={'200px'}
-        alt='dog image'
-      />
-
+    <div className="card">
+      <img src={user.avatar} alt={`${user.first_name} ${user.last_name}`} />
+      <div className="details">
+        <h2>{`${user.first_name} ${user.last_name}`}</h2>
+        <p>{user.email}</p>
+        <p>{`ID: ${user.id}`}</p>
+      </div>
     </div>
-  )
-
+  );
 }
 
+function App() {
+  const [users, setUsers] = useState([]);
+
+  async function getUsers() {
+    try {
+      const response = await fetch('https://reqres.in/api/user');
+      const data = await response.json();
+      setUsers(data.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  return (
+    <div className="container">
+      <button onClick={getUsers}>Get Users</button>
+      <div className="card-container">
+        {users.map((user) => (
+          <UserCard key={user.id} user={user} />
+        ))}
+      </div>
+    </div>
+  );
+}
 
 export default App;
