9c3deaf27e28d8787ce7a05637d3ebece2b349cc
AshwiniKaradge <ashwinikaradge2018@gmail.com>
2023-05-03 10:56:47 +0530

    this is assignment on featch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, {useState, useEffect} from 'react';
 
 function App() {
+
+  const [image , setImage] = useState('https://images.pexels.com/photos/3177388/pexels-photo-3177388.jpeg?auto=compress&cs=tinysrgb&w=800')
+  const url="http://localhost:3001/"
+  const WAIT_TIME = 2000;
+  useEffect(()=> {
+    const id= setInterval(()=>{
+      setImage
+      .get(url)
+      .then(res=>{
+        setImage(res.data)
+      })
+      .catch(err=>{
+        console.log(err);
+      })
+    }, WAIT_TIME);
+    return ()=> clearInterval(id)
+  }, [image]);
+  async function getDogData () {
+    
+    fetch('https://dog.ceo/api/breeds/image/random')
+    .then((data)=> data.json())
+    .then ((response)=> setImage(response.message))
+    .catch((error) => console.log(error))  
+
+  }
+
+  async function getDog () {
+
+    try{
+          const response = await fetch('https://dog.ceo/api/breeds/image/random')
+        const data = await response.json()
+        setImage(data.message)
+    }
+    catch(error){
+      console.log('i am from catch' ,error)
+    }
+
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img 
+        src={image}
+        height={'200px'}
+        width={'200px'}
+        alt='dog image'
+      />
+
     </div>
-  );
+  )
+
 }
+
 
 export default App;
