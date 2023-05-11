5d7d88ad5d3c192f5b44f1494a5ea59b4695560d
AshwiniKaradge <ashwinikaradge2018@gmail.com>
2023-05-11 11:13:27 +0530

    this is scroll bar assignment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from "./App.module.css"
+import { Scrollbar } from './molecule/Scrollbar';
+import { useState ,useEffect} from 'react';
 
 function App() {
+  const [arr , setArr] = useState([])
+
+
+ let api = "https://reqres.in/api/users/"
+
+useEffect(() =>{
+  fetch(api)
+  .then((response) => response.json())
+  .then((actualData) =>{
+    console.log(actualData.data)
+    setArr(actualData.data)
+  }).catch((err)=>console.log(err))
+},[])
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.App}>
+     {arr.map((Element)=>{
+      return <Scrollbar
+      key={Element.id}
+      elements = {Element}
+      />
+     })}
     </div>
   );
 }
---
+++ src/molecule/Scrollbar.js
@@ -0,0 +1,17 @@
+import styles from "./Scrollbar.module.css"
+
+export const Scrollbar= ({elements}) => {
+
+
+    return(
+        <div className={styles.ProfileContainer}>
+        
+        <img src={elements.avatar} alt=""/>
+
+       <h1>{elements.id}</h1>
+        <h2>{elements.email}</h2>
+        <h1>{elements.first_name}  {elements.last_name}</h1>
+        
+       </div>
+    )
+}
